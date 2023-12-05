f97b5eedfca4d37e0269db8898f66f3d4f72e198
dmanzanod <dmanzanod@gmail.com>
2023-12-05 15:24:27 -0300

    Update config.php

--- phpgrid/config.php
+++ phpgrid/config.php
@@ -3,9 +3,9 @@
 // PHP Grid database connection settings, Only need to update these in new project
 
 define("PHPGRID_DBTYPE","mysqli"); // mysql,oci8(for oracle),mssql,postgres,sybase
-define("PHPGRID_DBHOST","ec2-52-90-113-228.compute-1.amazonaws.com");
-define("PHPGRID_DBUSER","zazudb2");
-define("PHPGRID_DBPASS","zazu2023");
+define("PHPGRID_DBHOST","roundhouse.proxy.rlwy.net:23957");
+define("PHPGRID_DBUSER","root");
+define("PHPGRID_DBPASS","dDbFeEd4C5-5-C5fgFb4ECdEbhD5A5C6");
 define("PHPGRID_DBNAME","bd_andree");
 
 // database charset
